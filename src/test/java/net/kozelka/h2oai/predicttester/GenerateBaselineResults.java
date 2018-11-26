9fc0c450d9aec1c18c59fe5181e1999d13212cb8
Petr Kozelka <pkozelka@gmail.com>
2018-11-26 16:16:45 +0100

    added generator of baseline results, in form of a dynamic junit test

---
+++ src/test/java/net/kozelka/h2oai/predicttester/GenerateBaselineResults.java
@@ -0,0 +1,45 @@
+package net.kozelka.h2oai.predicttester;
+
+import org.codehaus.plexus.util.FileUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@RunWith(Parameterized.class)
+public class GenerateBaselineResults {
+    private static final File DATASETS = new File("data");
+    private static final File RESULT = new File("target/result");
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Iterable<? extends String> deepFiles() throws IOException {
+        final List<String> models = FileUtils.getDirectoryNames(DATASETS, "*/models/*v?.??_*.mojo.d", null, false);
+        return models;
+    }
+
+    @Parameterized.Parameter
+    public String modelPath;
+
+    @Test
+    public void generateBaseline() throws Exception {
+        final String[] mpa = modelPath.split("/");
+        final String datasetName = mpa[0];
+
+        final File csvFile = new File(DATASETS, String.format("%1$s/%1$s.csv", datasetName));
+        final File resultFile = new File(RESULT, String.format("%s/%s.tsv",
+            datasetName,
+            mpa[2].replace(".mojo.d", "")
+        ));
+        resultFile.getParentFile().mkdirs();
+
+        final H2oPredictTester pt = new H2oPredictTester(new File(DATASETS, modelPath));
+        final long start = System.currentTimeMillis();
+        pt.predictAll(csvFile, resultFile);
+        long duration = System.currentTimeMillis() - start;
+        System.out.printf("%s: %d millis%n", modelPath, duration);
+    }
+
+}
